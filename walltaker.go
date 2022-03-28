b1b748c9f99fea8da812a9d3c86acd63c2958bf4
Astra Bun <78638049+oddpawsx@users.noreply.github.com>
2022-03-28 19:22:20 -0400

    logs stdout/stderr to file

--- walltaker.go
+++ walltaker.go
@@ -214,6 +214,9 @@ func openE621(postUrl string) {
 }
 
 func main() {
+	// log to file
+	fn := logOutput()
+	defer fn()
 	// use file lock to determine if walltaker is already running
 	lockPath := "./walltaker.lock"
 	if runtime.GOOS == "darwin" {
@@ -574,4 +577,63 @@ func onReady() {
 			}
 		}
 	}()
+}
+
+func logOutput() func() {
+	// modified from https://gist.github.com/jerblack/4b98ba48ed3fb1d9f7544d2b1a1be287
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		panic(err)
+	}
+	wtCacheDir := filepath.Join(cacheDir, ".walltaker")
+	wtCacheLogsDir := filepath.Join(wtCacheDir, "logs")
+	if _, err := os.Stat(wtCacheDir); os.IsNotExist(err) {
+		err := os.Mkdir(wtCacheDir, os.FileMode(0777))
+		if err != nil {
+			panic(err)
+		}
+	}
+	if _, err := os.Stat(wtCacheLogsDir); os.IsNotExist(err) {
+		err := os.Mkdir(wtCacheLogsDir, os.FileMode(0777))
+		if err != nil {
+			panic(err)
+		}
+	}
+	logfile := filepath.Join(wtCacheLogsDir, "walltaker.log")
+	// open file read/write | create if not exist | clear file at open if exists
+	f, _ := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+
+	// save existing stdout | MultiWriter writes to saved stdout and file
+	out := os.Stdout
+	mw := io.MultiWriter(out, f)
+
+	// get pipe reader and writer | writes to pipe writer come out pipe reader
+	r, w, _ := os.Pipe()
+
+	// replace stdout,stderr with pipe writer | all writes to stdout, stderr will go through pipe instead (fmt.print, log)
+	os.Stdout = w
+	os.Stderr = w
+
+	// writes with log.Print should also write to mw
+	log.SetOutput(mw)
+
+	//create channel to control exit | will block until all copies are finished
+	exit := make(chan bool)
+
+	go func() {
+		// copy all reads from pipe to multiwriter, which writes to stdout and file
+		_, _ = io.Copy(mw, r)
+		// when r or w is closed copy will finish and true will be sent to channel
+		exit <- true
+	}()
+
+	// function to be deferred in main until program exits
+	return func() {
+		// close writer then block on exit channel | this will let mw finish writing before the program exits
+		_ = w.Close()
+		<-exit
+		// close file after all writes have finished
+		_ = f.Close()
+	}
+
 }
