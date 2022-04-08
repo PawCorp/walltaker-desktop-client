81690185bdea403565fad0d0c8a6cbe04188b221
Astra Bun <78638049+oddpawsx@users.noreply.github.com>
2022-04-07 21:50:25 -0400

    v2.0.3

--- walltaker.go
+++ walltaker.go
@@ -30,7 +30,7 @@ import (
 	"github.com/reujab/wallpaper"
 )
 
-var VERSION string = "v2.0.2"
+var VERSION string = "v2.0.3"
 
 type WalltakerData struct {
 	ID               int         `json:"id"`
